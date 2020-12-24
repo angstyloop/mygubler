b0590422a43ecb2ae88188a57185c5db46bc5003
Sean Allen <sean.allen@dfstudio.com>
2020-12-24 05:43:46 -0800

    gag gift for lea for christmas - a picture of MGG (Matthew Gray Gubler) with some poetry. Right now its a Sappho quote.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Matthew Gray Gubler" src="./assets/mgg_1.jpg">
+    <div class="text">{{quote}}</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      quote: "Love shook my heart like the wind on the mountain rushing over the oak trees."
+    }
+  },
 }
 </script>
 
 <style>
 #app {
+  background-color: black;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  position: relative;
+  /*margin-top: 60px;*/
+}
+.text {
+  position: absolute;
+  bottom: 15%;
+  font-size: 8vh;
+  color: white;
+  width: auto;
+}
+img {
+  opacity: .8;
+  width: auto;
+  max-width: 100%;
+  height: auto;
 }
 </style>
